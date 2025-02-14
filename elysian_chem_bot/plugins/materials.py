993ac0834f67366ce5440f1a622b66a9733a691e
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2025-02-15 02:49:04 +0800

    feat(plugins/materials): use inline keyboard instead of force reply keyboard
    * looks nicer, less spam

--- elysian_chem_bot/plugins/materials.py
+++ elysian_chem_bot/plugins/materials.py
@@ -14,228 +14,140 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 import logging
-import zipfile
 from hashlib import md5
-from pathlib import Path
+from itertools import batched
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 from typing import Any, cast
+from zipfile import ZipFile, is_zipfile
 
-from anyio import open_file
+from anyio import Path, open_file
 from jsondb.database import JsonDB
 from pyrogram.client import Client
-from pyrogram.enums import MessageMediaType
-from pyrogram.filters import command, create
-from pyrogram.types import KeyboardButton, Message, ReplyKeyboardMarkup
+from pyrogram.filters import command
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from elysian_chem_bot import DB_PERSIST_PATH, cmdhelp_instance, db_instance
+from elysian_chem_bot import DB_PERSIST_PATH, db_instance
 from elysian_chem_bot.database_types import File, Sections
-from elysian_chem_bot.utils import sanitize_message
 
 log: logging.Logger = logging.getLogger(__name__)
-msg_to_be_watched: list[tuple[int, list[str]]] = []
 cache_db: JsonDB = JsonDB(Path(Path(DB_PERSIST_PATH).parent).joinpath("extracted_files_cache.json").as_posix())
 
-# Migrate our cache DB from sha1 to md5
-if not cache_db.data.get("hash_type"):
-    log.info("migrating cache database from sha1 to md5, discarding old sha1 caches")
-    cache_db.data.clear()
-    cache_db.data.update({"hash_type": "md5"})
+
+class MaterialCallbackData:
+    def __init__(self, callback_string: str) -> None:
+        """Callback data for the button of the material.
+
+        Args:
+            callback_string (str): "MUST be sections delimited by /:file name"
+        """
+        self.sections: list[str] = callback_string.split(":")[0].split("/")
+        self.file_name_or_section: str = callback_string.split(":")[1]
+
+        if self.sections[0] == "":
+            self.sections.pop(0)
+
+    def __str__(self) -> str:
+        """Returns a string representation of the callback data of a material."""
+        return f"{'/'.join(self.sections)}:{self.file_name_or_section}"
 
 
-async def should_be_handled(_, __, message: Message) -> bool:
-    if not message.reply_to_message:
-        return any(message.chat.id == x[0] for x in msg_to_be_watched)
-    return any(message.reply_to_message.id == x[0] for x in msg_to_be_watched)
-
-
-async def get_buttons(sections: Sections) -> list[str]:
+async def generate_inline_keyboard_markup(sections: Sections, columns: int = 3) -> InlineKeyboardMarkup:
     db_instance.raw_db = cast(dict[str, dict[str, Any]], db_instance.raw_db)
 
-    cur_section: dict[str, dict[str, Any]] = db_instance.raw_db
+    # Get to the requested section first
+    cur_sec = db_instance.raw_db
     for sec in sections:
-        cur_section = cur_section[sec]
+        cur_sec = cur_sec[sec]
 
-    return list(cur_section)
+    # Now we generate the buttons
+    # note: callback data will be "sections delimited with /:filename"
+    rows: list[list[InlineKeyboardButton]] = []
+    joined_sections = "/".join(sections)
+    for batch in batched(cur_sec, columns):
+        keyboardified_batch: list[InlineKeyboardButton] = [
+            InlineKeyboardButton(x, callback_data=f"{joined_sections}:{x}") for x in batch
+        ]
+        rows.append(keyboardified_batch)
+
+    return InlineKeyboardMarkup(rows)
 
 
-async def auto_extract_zip_archive(client: Client, message: Message, file_id: str) -> None:
-    msg = await message.reply_text("Automatically extracting zip archive...")
+@Client.on_message(command("bahanbeta"))
+async def material_beta(client: Client, message: Message) -> None:
+    inline_keyboard: InlineKeyboardMarkup = await generate_inline_keyboard_markup([])
+    await message.reply_text("Please use the button below\n**Current section is:** __/__", reply_markup=inline_keyboard)
+
+
+@Client.on_callback_query(group=2)
+async def material_cb(client: Client, cb_query: CallbackQuery) -> None:
+    material_callback: MaterialCallbackData = MaterialCallbackData(cast(str, cb_query.data))
+
+    log.info("sections=%s", material_callback.sections)
+    log.info("fileid=%s", material_callback.file_name_or_section)
+
+    sections_plus_file_name: list[str] = material_callback.sections.copy()
+    sections_plus_file_name.append(material_callback.file_name_or_section)
+
+    if not db_instance.is_sections_exist(sections_plus_file_name).status:
+        # then it is a file, upload the file and return early
+        file: File = db_instance.get_file(material_callback.sections, material_callback.file_name_or_section)
+        chat_id = cb_query.message.chat.id
+        user_name: str = cb_query.from_user.first_name
+        user_id: int = cb_query.from_user.id
+        suffix: str = ""
+        if material_callback.file_name_or_section.endswith(".zip"):
+            suffix = "**reply to this file with** /unzip **to unpack this archive**"
+
+        await client.send_document(chat_id, file.file_id, caption=f"[{user_name}](tg://user?id={user_id}) {suffix}")
+        return
+
+    inline_keyboard: InlineKeyboardMarkup = await generate_inline_keyboard_markup(sections_plus_file_name)
+    await cb_query.message.edit(
+        f"Please use the button below\n**Current section is:** __{'/'.join(sections_plus_file_name)}__",
+        reply_markup=inline_keyboard,
+    )
+
+
+@Client.on_message(command("unzip"))
+async def unzip(client: Client, message: Message) -> None:
+    if not message.reply_to_message or not message.reply_to_message.document:
+        await message.reply_text("Please reply to a file!")
+        return
+
+    file_id: str = message.reply_to_message.document.file_id
+    msg: Message = await message.reply_text("__Processing file...__")
+
     with NamedTemporaryFile() as tf:
         log.info("created temporary file '%s'", tf.name)
 
         log.info("downloading document with file_id '%s'", file_id)
         await msg.edit_text(f"**downloading document with file_id** `'{file_id}'`")
         await client.download_media(file_id, tf.name)
-        log.info("document downloaded")
+
+        if not is_zipfile(tf.name):
+            await msg.edit_text("That file is not a zip file!")
+            return
 
         with TemporaryDirectory() as td:
-            log.info("created temporary directory '%s' for archive extraction", td)
-            log.info("**extracting archive**")
-            await msg.edit_text("Extracting the zip archive...")
-            zipfile.ZipFile(tf.name).extractall(td)
-            log.info("archive extracted")
+            zip_file = ZipFile(tf.name)
+            zip_file.extractall(td)
 
-            log.info("uploading extracted files")
-            for file in Path(td).rglob("*"):
-                if file.is_file():
-                    log.info("uploading file '%s'", file.as_posix())
-                    await msg.edit_text(f"**uploading file** `'{file.as_posix()}'`")
-                    async with await open_file(file.as_posix(), "rb") as f:
-                        content = await f.read()
-                        md5sum: str = md5(content).hexdigest()  # noqa: S324
-                        if cached_file_id := cache_db.data.get(md5sum):
-                            log.info("file '%s' found in cache, re-using file_id", file.as_posix())
-                            await message.reply_document(cached_file_id, file_name=file.name)
-                        else:
-                            log.info("file '%s' NOT found in cache, uploading instead", file.as_posix())
-                            doc = await message.reply_document(file.as_posix(), file_name=file.name)
-                            log.info("storing file '%s' in cache", file.as_posix())
-                            cache_db.data.update({md5sum: doc.document.file_id})
+    async for file in Path(td).rglob("*"):
+        if not file.is_file:
+            return
 
-            await msg.delete()
+        await msg.edit_text(f"Uploading file {file.as_posix()}")
 
+        async with await open_file(file.as_posix(), "rb") as extracted_file:
+            content: bytes = await extracted_file.read()
+            md5_sum: str = md5(content).hexdigest()  # noqa: S324
+            if cached_file_id := cache_db.data.get(md5_sum):
+                log.info("file '%s' found in cache, re-using file_id", file.as_posix())
+                await message.reply_document(cached_file_id)
+            else:
+                log.info("file '%s' NOT found in cache, uploading instead", file.as_posix())
+                doc = await message.reply_document(file.as_posix(), file_name=file.name)
 
-@Client.on_message(create(should_be_handled), group=1)
-async def handle_reply(client: Client, message: Message) -> None:
-    global msg_to_be_watched
-
-    if message.text.lower() in ["/bahan", "/material"]:
-        return
-
-    if message.from_user.id == message.chat.id:
-        sections: Sections = next(x[1] for x in msg_to_be_watched if x[0] == message.from_user.id)
-    else:
-        sections: Sections = next(x[1] for x in msg_to_be_watched if x[0] == message.reply_to_message.id)
-    sections.append(message.text)
-
-    if not db_instance.is_sections_exist(sections).status:
-        sections.pop()
-        file: File = db_instance.get_file(sections, message.text)
-        await message.reply_document(file.file_id)
-
-        if message.text.endswith(".zip"):
-            log.info("file ends with .zip, extracting")
-            await auto_extract_zip_archive(client, message, file.file_id)
-
-        return
-
-    material_buttons: list[str] = await get_buttons(sections)
-    buttons_rows: list[list[KeyboardButton | str]] = []
-
-    while len(material_buttons) > 0:
-        try:
-            current_buttons: list[str] = material_buttons[0:2]
-            converted: list[KeyboardButton | str] = [KeyboardButton(x) for x in current_buttons]
-            material_buttons = material_buttons[2:]
-        except IndexError:
-            converted: list[KeyboardButton | str] = [KeyboardButton(material_buttons[0])]
-            material_buttons.pop(0)
-
-        buttons_rows.append(converted)
-
-    log.info("[reply handler] rows: %s", buttons_rows)
-    if len(buttons_rows) == 0:
-        log.warning("empty rows!")
-        await message.reply_text("this section is empty!")
-        return
-
-    msg = await message.reply_text(
-        f"Please use the (now changed) button\nnow in section: __{'/'.join(sections)}__",
-        reply_markup=ReplyKeyboardMarkup(buttons_rows),
-    )
-
-    if any(message.chat.id == x[0] for x in msg_to_be_watched):
-        rem: Sections = sections.copy()
-        rem.pop()
-        msg_to_be_watched = list(filter(lambda x: x[0] != message.from_user.id, msg_to_be_watched))
-        msg_to_be_watched.append((message.from_user.id, sections))
-    else:
-        msg_to_be_watched.append((msg.id, sections))
-
-
-@Client.on_message(command(["bahan", "material"]))
-async def material(client: Client, message: Message) -> None:
-    global msg_to_be_watched
-
-    material_buttons: list[str] = await get_buttons([])
-    buttons_rows: list[list[KeyboardButton | str]] = []
-
-    while len(material_buttons) > 0:
-        try:
-            current_buttons = material_buttons[0:2]
-            btns: list[KeyboardButton | str] = [KeyboardButton(text=x) for x in current_buttons]
-            material_buttons = material_buttons[2:]
-        except IndexError:
-            btns: list[KeyboardButton | str] = [KeyboardButton(text=material_buttons[0])]
-            material_buttons.pop(0)
-
-        buttons_rows.append(btns)
-
-    log.info("rows: %s", buttons_rows)
-    msg = await message.reply_text(
-        "Please use the button below to make selection",
-        reply_markup=ReplyKeyboardMarkup(buttons_rows),
-    )
-
-    if message.chat.id > 0:
-        msg_to_be_watched = list(filter(lambda x: x[0] != message.from_user.id, msg_to_be_watched))
-        msg_to_be_watched.append((message.chat.id, []))
-    else:
-        msg_to_be_watched.append((msg.id, []))
-
-
-@Client.on_message(command(["addmaterial", "addbahan"]))
-async def add_material(client: Client, message: Message) -> None:
-    clean_text: str = await sanitize_message(message.text, ["addmaterial", "addbahan"])
-    sections: Sections = clean_text.split("/")
-
-    if not db_instance.is_sections_exist(sections).status:
-        log.error("sections '%s' does not exist", sections)
-        await message.reply_text("section does not exist")
-        return
-
-    if not message.reply_to_message:
-        await message.reply_text("reply to a message!")
-        return
-
-    if message.reply_to_message.media not in (
-        MessageMediaType.DOCUMENT,
-        MessageMediaType.PHOTO,
-        MessageMediaType.VIDEO,
-    ):
-        await message.reply_text("invalid media type!")
-        return
-
-    # check if it's an album
-    try:
-        medias: list[Message] = await message.reply_to_message.get_media_group()
-    except ValueError:
-        medias: list[Message] = [message.reply_to_message]
-
-    for media in medias:
-        log.info(
-            "adding file '%s', where file id: '%s' file unique id: '%s'",
-            media.document.file_name,
-            media.document.file_id,
-            media.document.file_unique_id,
-        )
-        db_instance.add_file(sections, media.document.file_name, media.document.file_id, media.document.file_unique_id)
-
-    mantap = [f"__{x.document.file_name}__" for x in medias]
-    await message.reply_text(f"added {', '.join(mantap)} to section **{sections}**")
-
-
-@Client.on_message(command("dumpcache"))
-async def dump_cache(client: Client, message: Message) -> None:
-    with NamedTemporaryFile("w+", suffix=".json") as f:
-        async with await open_file(f.name, "w", encoding="utf-8") as wf:
-            content = json.dumps(cache_db.data, indent=4)
-            await wf.write(content)
-
-        await message.reply_document(f.name)
-
-
-cmdhelp_instance.add_commands(["bahan", "material"], "Get materials")
-cmdhelp_instance.add_commands("dumpcache", "dump the cache of extracted files")
+                log.info("storing file '%s' in cache", file.as_posix())
+                cache_db.data.update({md5_sum: doc.document.file_id})
